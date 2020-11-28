42ee9298c1db5656c58b458fd8674f721b1d5bb2
Potchara Puttawanchai <potchangelo@gmail.com>
2020-11-28 22:11:00 +0700

    JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const buttonText = 'สักหน่อยมั้ยหละ';
+    const okButton = (
+        <button>
+            {buttonText}
+        </button>
+    );
+    const textInput = (
+        <input type="text" />
+    );
+
+    return (
+        <div className="app">
+            <h2>เจ็บปวดที่งดงาม</h2>
+            <p>อิอิอิอิ</p>
+            <p>
+                {textInput}
+            </p>
+            <p>
+                {okButton}
+            </p>
+        </div>
+    );
 }
 
 export default App;
