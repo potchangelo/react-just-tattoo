ca7c12662ba34097706339711d4d653531971e34
Potchara Puttawanchai <potchangelo@gmail.com>
2022-04-26 15:40:41 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Just Tattoo', () => {
   render(<App />);
   const AppHeaderLogo = screen.getByAltText(/Just Tattoo/i);
   expect(AppHeaderLogo).toBeInTheDocument();
