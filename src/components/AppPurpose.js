5dcc81e43e8746bccbb297faa6c92bf1028dfc24
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-23 13:15:18 +0700

    Purpose banner as a component

---
+++ src/components/AppPurpose.js
@@ -0,0 +1,14 @@
+function AppPurpose() {
+  return (
+    <header class="purpose-banner">
+      <div class="purpose-banner-item">
+        <span>Note : โปรเจ็คเว็บนี้ทำขึ้นมาเพื่อการศึกษาทางการเขียนโปรแกรมเท่านั้น เนื้อหาบนเว็บไม่ใช่สินค้าหรือบริการที่มีอยู่จริง</span>
+      </div>
+      <div class="purpose-banner-item">
+        <a href="https://github.com/potchangelo/react-just-tattoo" target="_blank" rel="noreferrer">Project code on Github</a>
+      </div>
+    </header>
+  );
+}
+
+export default AppPurpose;
